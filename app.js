802b3571fbc32fc166007d9360166384137c75c7
Deepacks <vladimirsette.vc@gmail.com>
2021-07-07 17:18:13 +0200

    cors for dev

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://flavorite.vercel.app",
+    origin: "http://localhost:3000",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
     credentials: true,
