db7f401d128cf7bb9f9bdb485bb227c1b8c8429f
Deepacks <vladimirsette.vc@gmail.com>
2021-07-08 15:55:13 +0200

    cors test

--- cors.js
+++ cors.js
@@ -2,10 +2,7 @@ const cors = (req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if ("OPTIONS" == req.method) {
     res.send(200);
   } else {
