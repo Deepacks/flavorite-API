5164c203bbfdbe267e5a6787c4fffcb50179f7f7
Deepacks <vladimirsette.vc@gmail.com>
2021-07-08 15:38:55 +0200

    cors test

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors);
 
 // -------------- PASSPORT CONFIG --------------
 
