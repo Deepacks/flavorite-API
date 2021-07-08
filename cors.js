f96af3cb86fde446a48789f562cc1ba40cd48331
Deepacks <vladimirsette.vc@gmail.com>
2021-07-08 15:48:48 +0200

    cors test

--- cors.js
+++ cors.js
@@ -1,16 +1,15 @@
 const cors = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Authorization, WWW-Authenticate, Cookie, Set-Cookie, Accept"
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
   );
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header(
-    "Access-Control-Request-Headers",
-    "Origin, X-Requested-With, Content-Type, Authorization, WWW-Authenticate, Cookie, Set-Cookie, Accept"
-  );
-  res.header("Access-Control-Request-Method", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
 
   next();
 };
